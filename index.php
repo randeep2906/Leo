baa2d2a157bd03af7102c8c8bd28511f70409715
Harshit Pandey <harshitp@HarshitP-M1.local>
2016-06-21 01:53:01 -0700

    Added CORS

--- index.php
+++ index.php
@@ -1,1 +1,4 @@
 <?php include_once("index.html"); ?>
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
